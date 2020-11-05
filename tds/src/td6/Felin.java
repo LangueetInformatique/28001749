fe6cd11031cc38cea28820021d6ab6bf080c4d01
bezancon <bezancon@bezancon-E403NA>
2020-11-05 11:00:08 +0100

    td6 félins

---
+++ tds/src/td6/Felin.java
@@ -0,0 +1,26 @@
+package td6;
+
+/** Creation et gestion de felins */
+public class Felin extends Animal {
+// ajout d'attributs propres a la sous-classe
+	protected boolean domestique = false;
+
+	/**
+	 * le constructeur de Felins fait appel au constructeur de la sur-classe Animal
+	 */
+	public Felin(String type, int nb_pattes) { // les felins ont 4 pattes
+		super("felin",4);
+	}					
+
+	/** presentation des caracteristiques du felin */
+	public void presente() {
+		
+		String etat = (domestique) ? "domestique" : "sauvage";
+		System.out.println("je suis vraiment un animal " + etat);
+	}
+
+	/** cri du felin */
+	public void crie() {
+		System.out.println("et je miaule fort");
+	}
+}
