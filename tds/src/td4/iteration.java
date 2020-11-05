1eaa566c5b5eecbc974e90cca062f585a54cac8b
bezancon <bezancon@bezancon-E403NA>
2020-11-05 09:43:16 +0100

    ensemble des tds effectués

---
+++ tds/src/td4/iteration.java
@@ -0,0 +1,18 @@
+package td4;
+
+public class iteration {
+
+	public static void main(String[]args) {
+		
+		String[] prenoms = {"Christian","Karen","Victoria","Agatha","Phillipe","Djamé"};
+		int imin;
+		
+		for(int i=0; i<prenoms.length;i++) {
+		
+		System.out.println(prenoms[0].compareTo(prenoms[i]));
+		
+		}
+		
+	}
+	
+}
