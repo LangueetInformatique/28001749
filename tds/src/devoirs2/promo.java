3398b432554d8bfa2c9b51f45bd0aa31cdc36bcd
bezancon <bezancon@bezancon-E403NA>
2020-11-25 13:14:04 +0100

    update et correction d'une erreur

--- tds/src/devoirs2/promo.java
+++ tds/src/devoirs2/promo.java
@@ -8,6 +8,7 @@ public class promo {
 	 * fonctionnalités principales d'une promotion étudiante:
 	 * 		l'année de la promotion
 	 * 		les etudiants qu'il y a dedans
+	 * 		une moyenne
 	 */
 	
 	int annee;
@@ -27,6 +28,20 @@ public class promo {
 	public int get_nb_etudiant() {
 		
 		return etudiants.size();
+	}
+	
+	public float moyenne() {
+		
+		float total = 0;
+		int nb_etudiant = get_nb_etudiant();
+		
+		for(int i =0;i<nb_etudiant;i++) {
+			
+			total = total + etudiants.get(i).getnote();
+		}
+		
+		total = total/nb_etudiant;
+		return total;
 	}
 	
 }
---
+++ tds/src/devoirs4/calendar.java
@@ -0,0 +1,5 @@
+package devoirs4;
+
+public class calendar {
+
+}
